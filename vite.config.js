039ea88ef2f40ad5526e7cdc30c31f92c360444f
Kauan Rodrigues <kauanrdx145@gmail.com>
2025-11-23 10:21:06 -0300

    Dockerfile e configuração de porta

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    port: 7006,
+    host: '0.0.0.0',
+    // allowedHosts: [''],
+    cors: true,
+  },
+  preview: {
+    // allowedHosts: [''],
+    cors: true,
   }
 })
